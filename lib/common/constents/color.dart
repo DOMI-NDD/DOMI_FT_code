6cabf38ee272f320925ff046a47a87c128013bcc
최승우 <choi0250655@gmail.com>
2025-09-30 17:32:01 +0900

    color 추가
    color 추가

---
+++ lib/common/constents/color.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+abstract final class DomiColor{
+  static const Color wrong = Color(0xFFFF7474);
+  static const Color warn = Color(0xFFFFDD54);
+  static const Color correct = Color(0xFF54FF7F);
+  static const Color primary = Color(0xFF454CFF);
+  static const Colorsecondary = Color(0xFF6D71FF);
+  static const Color gray50 = Color(0xFFF4F4F4);
+  static const Color gray100 = Color(0xFFE4E4E4);
+  static const Color gray200 = Color(0xFFD0D0D0);
+  static const Color gray300 = Color(0xFFBCBCBC);
+  static const Color gray400 = Color(0xFFA7A7A7);
+  static const Color gray500 = Color(0xFF939393);
+  static const Color gray600 = Color(0xFF7E7E7E);
+  static const Color gray700 = Color(0xFF6A6A6A);
+  static const Color gray800 = Color(0xFF565656);
+}
